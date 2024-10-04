d3e6a2589096d16fede14df7fb7d56027a023c25
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2024-10-04 02:18:52 -0300

    .

--- microsservicousuario/src/main/java/br/com/mateus/microsservicousuario/model/Produto.java
+++ microsservicousuario/src/main/java/br/com/mateus/microsservicousuario/model/Produto.java
@@ -1,5 +1,17 @@
 package main.java.br.com.mateus.microsservicousuario.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
-    
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Double valor;
 }
